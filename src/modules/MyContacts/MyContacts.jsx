56a8a64a60668e0324582bf58a8acc308f245a2f
Bogdan <bogdanberezka@gmail.com>
2023-02-25 10:47:16 +0200

    remove-adding-props-for-mycontactform-component

--- src/modules/MyContacts/MyContacts.jsx
+++ src/modules/MyContacts/MyContacts.jsx
@@ -4,36 +4,32 @@ import MyContactsForm from './MyContactsForm/MyContactsForm';
 import MyContactList from './MyContactList/MyContactList';
 import MyContactsFilter from './MyContactsFilter/MyContactsFilter';
 
-import { addContact, deleteContact } from '../../redux/contacts/contacts-slice';
+import { deleteContact } from '../../redux/contacts/contacts-slice';
 import { setFilter } from '../../redux/filter/filter-slice';
 
-import {
-  getAllContacts,
-  getFilteredContacts,
-} from '../../redux/contacts/contacts-selectors';
+import { getFilteredContacts } from '../../redux/contacts/contacts-selectors';
 import { getFilter } from '../../redux/filter/filter-selectors';
 
 const MyContacts = () => {
   const filteredContacts = useSelector(getFilteredContacts);
-  const allContacts = useSelector(getAllContacts);
   const filter = useSelector(getFilter);
 
   const dispatch = useDispatch();
 
-  const handleAddContact = ({ name, number }) => {
-    const normalaizedName = name.toLowerCase();
+  // const handleAddContact = ({ name, number }) => {
+  //   const normalaizedName = name.toLowerCase();
 
-    if (
-      allContacts.find(
-        contact => contact.name.toLowerCase() === normalaizedName
-      )
-    ) {
-      alert(`${name} is already in contacts`);
-      return false;
-    }
+  //   if (
+  //     allContacts.find(
+  //       contact => contact.name.toLowerCase() === normalaizedName
+  //     )
+  //   ) {
+  //     alert(`${name} is already in contacts`);
+  //     return false;
+  //   }
 
-    dispatch(addContact({ name, number }));
-  };
+  //   dispatch(addContact({ name, number }));
+  // };
 
   const handleDeleteContact = id => {
     dispatch(deleteContact(id));
@@ -48,7 +44,7 @@ const MyContacts = () => {
   return (
     <div>
       <h1>Phonebook</h1>
-      <MyContactsForm onSubmit={handleAddContact} />
+      <MyContactsForm />
       <h2>Contacts</h2>
       <MyContactsFilter value={filter} onChange={handleFilter} />
       {isContacts && (
@@ -63,3 +59,70 @@ const MyContacts = () => {
 };
 
 export default MyContacts;
+
+///////////////Ось так все було і все працювало чудово/////////////////////////
+// import { useSelector, useDispatch } from 'react-redux';
+
+// import MyContactsForm from './MyContactsForm/MyContactsForm';
+// import MyContactList from './MyContactList/MyContactList';
+// import MyContactsFilter from './MyContactsFilter/MyContactsFilter';
+
+// import { addContact, deleteContact } from '../../redux/contacts/contacts-slice';
+// import { setFilter } from '../../redux/filter/filter-slice';
+
+// import {
+//   getAllContacts,
+//   getFilteredContacts,
+// } from '../../redux/contacts/contacts-selectors';
+// import { getFilter } from '../../redux/filter/filter-selectors';
+
+// const MyContacts = () => {
+//   const filteredContacts = useSelector(getFilteredContacts);
+//   const allContacts = useSelector(getAllContacts);
+//   const filter = useSelector(getFilter);
+
+//   const dispatch = useDispatch();
+
+//   const handleAddContact = ({ name, number }) => {
+//     const normalaizedName = name.toLowerCase();
+
+//     if (
+//       allContacts.find(
+//         contact => contact.name.toLowerCase() === normalaizedName
+//       )
+//     ) {
+//       alert(`${name} is already in contacts`);
+//       return false;
+//     }
+
+//     dispatch(addContact({ name, number }));
+//   };
+
+//   const handleDeleteContact = id => {
+//     dispatch(deleteContact(id));
+//   };
+
+//   const handleFilter = ({ target }) => {
+//     dispatch(setFilter(target.value));
+//   };
+
+//   const isContacts = Boolean(filteredContacts.length);
+
+//   return (
+//     <div>
+//       <h1>Phonebook</h1>
+//       <MyContactsForm onSubmit={handleAddContact} />
+//       <h2>Contacts</h2>
+//       <MyContactsFilter value={filter} onChange={handleFilter} />
+//       {isContacts && (
+//         <MyContactList
+//           contacts={filteredContacts}
+//           removeBook={handleDeleteContact}
+//         />
+//       )}
+//       {!isContacts && <p>No contacts in list, yet</p>}
+//     </div>
+//   );
+// };
+
+// export default MyContacts;
--- src/modules/MyContacts/MyContactsForm/MyContactsForm.jsx
+++ src/modules/MyContacts/MyContactsForm/MyContactsForm.jsx
@@ -1,12 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { addContact } from 'redux/contacts/contacts-slice';
+
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 
 import initialState from './initialState';
 
 import styles from './myContactsForm.module.css';
 
-const MyContactsForm = ({ onSubmit }) => {
+import { getAllContacts } from 'redux/contacts/contacts-selectors';
+
+const MyContactsForm = () => {
   const [state, setState] = useState({ ...initialState });
+  const allContacts = useSelector(getAllContacts);
+  const dispatch = useDispatch();
 
   const handleChange = ({ target }) => {
     const { name, value } = target;
@@ -16,9 +22,24 @@ const MyContactsForm = ({ onSubmit }) => {
     });
   };
 
+  const handleAddContact = ({ name, number }) => {
+    const normalaizedName = name.toLowerCase();
+
+    if (
+      allContacts.find(
+        contact => contact.name.toLowerCase() === normalaizedName
+      )
+    ) {
+      alert(`${name} is already in contacts`);
+      return false;
+    }
+
+    dispatch(addContact({ name, number }));
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
-    onSubmit({ ...state });
+    handleAddContact({ ...state });
     setState({ ...initialState });
   };
 
@@ -51,6 +72,60 @@ const MyContactsForm = ({ onSubmit }) => {
 };
 export default MyContactsForm;
 
-MyContactsForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
+///////////////Ось так все було і все працювало чудово/////////////////////////
+// import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+
+// import initialState from './initialState';
+
+// import styles from './myContactsForm.module.css';
+
+// const MyContactsForm = ({ onSubmit }) => {
+//   const [state, setState] = useState({ ...initialState });
+
+//   const handleChange = ({ target }) => {
+//     const { name, value } = target;
+//     const newValue = value;
+//     setState(prevState => {
+//       return { ...prevState, [name]: newValue };
+//     });
+//   };
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     onSubmit({ ...state });
+//     setState({ ...initialState });
+//   };
+
+//   const { name, number } = state;
+//   return (
+//     <form onSubmit={handleSubmit} className={styles.form}>
+//       <input
+//         value={name}
+//         onChange={handleChange}
+//         type="text"
+//         name="name"
+//         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//         required
+//       />
+//       <input
+//         value={number}
+//         onChange={handleChange}
+//         type="tel"
+//         name="number"
+//         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//         required
+//       />
+//       <button type="submit" className={styles.button}>
+//         Add contact
+//       </button>
+//     </form>
+//   );
+// };
+// export default MyContactsForm;
+
+// MyContactsForm.propTypes = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
